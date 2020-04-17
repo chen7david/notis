41f4764b3768a100e7aaf18b638a492b0043ffe4
David Chen <chen7david@me.com>
2020-04-17 16:50:56 +0800

    update

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-
 const Notis = require('./models/notis')
 const joi = require('./schema/joi')
-const _default = require('./schema/default')
+const generic = require('./schema/generic')
 const join = require('./schema/joined')
 
-const dd = (val) => console.log(val)
-
 module.exports = {
+
     notis: (schema) => new Notis(schema),
+
     notisexpress: (schema) => (req, _, next) => {
         req.notis = new Notis(schema)
         next()
     },
+
     schema: {
         joi,
-        default:_default,
+        generic,
         join 
     }
 }
-
-const notis = new Notis(_default)
-notis.load('duplicate', 'route', 'warning')
-dd(notis.load('forbidden', 'route', 'info'))
