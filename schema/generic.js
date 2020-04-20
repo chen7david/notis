027216dde60ac2501deeff7fe144d5219cdaa640
David <chen7david@me.com>
2020-04-20 23:54:30 +0800

    update

--- schema/generic.js
+++ schema/generic.js
@@ -20,7 +20,7 @@ module.exports = {
     disabled: (noun) =>`${noun} disabled`,
     missing: (noun) =>`${noun} missing`,
     confirmed: (noun) =>`${noun} confirmed`,
-    confirmation_required: (noun) =>`${noun} confirmation_required`,
+    confirmation_required: (noun) =>`${noun} confirmation required`,
     already_confirmed: (noun) =>`${noun} already confirmed`,
     custom: (noun) =>`${noun}`,
 
